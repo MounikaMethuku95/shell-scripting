afff696fbf751bfaa9be55a5f10797ace9bb3ab5
mounikamethuku <mounikamethuku95@gmail.com>
2024-12-30 19:54:17 +0530

    added missing colon

--- 04-variables.sh
+++ 04-variables.sh
@@ -3,7 +3,7 @@
 PERSON1=$1
 PERSON2=$2
 
-echo "$PERSON1: Hello $PERSON2"
-echo "$PERSON2: I'm fine $PERSON1"
-echo "$PERSON1: How was your day?"
-echo "$PERSON2: It was fine"
+echo "$PERSON1:: Hello $PERSON2"
+echo "$PERSON2:: I'm fine $PERSON1"
+echo "$PERSON1:: How was your day?"
+echo "$PERSON2:: It was fine"
