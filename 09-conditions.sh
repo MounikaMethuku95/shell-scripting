3647c5ba51385a5e21027b4fd2ca233384768287
mounikamethuku <mounikamethuku95@gmail.com>
2024-12-31 08:13:09 +0530

    conditions.sh

---
+++ 09-conditions.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+NUMBER1=$1
+NUMBER2=$2
+
+IF [ $NUMBER1 -gt $NUMBER2 ]
+then
+    echo "Given number is greater than $NUMBER2"
+else
+    echo "Given number is less than $NUMBER2"
+fi
