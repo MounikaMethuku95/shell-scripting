6896562cb7a1cce97da2295de74bedb585ea5fb9
mounikamethuku <mounikamethuku95@gmail.com>
2024-12-31 08:01:38 +0530

    arrays.sh

---
+++ 07-arrays.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+MOVIES=("kalki" "salaar" "BB3" "stree")
+
+
+echo "First movie: ${MOVIES[0]}"
+echo "Second movie: ${MOVIES[1]}"
+echo "Third movie: ${MOVIES[2]}"
+echo "Fourth movie is: ${MOVIES[3]}"
+
+echo "All movies are: ${MOVIES[@]}"
