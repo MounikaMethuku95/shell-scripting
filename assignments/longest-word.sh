a9e961dae4c878afb1b4fcdd2fb7a6022364d62d
mounikamethuku <mounikamethuku95@gmail.com>
2025-01-08 20:48:20 +0530

    find longest word

---
+++ assignments/longest-word.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+longword="strings.txt"
+G="\e[32m"
+for lw in $(<longword)
+do
+    len=${#lw}
+    if [ [ len -gr longword ]]
+    then 
+        longword=$len
+        longestword=$word
+    fi
+done
+echo " $G The longest word is %s with length %d.\n' "$longestword" "$longword"
