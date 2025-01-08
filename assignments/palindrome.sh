5250647aa1a3f527432c7a6cc9584c8fe6a28676
mounikamethuku <mounikamethuku95@gmail.com>
2025-01-08 18:53:50 +0530

    updated syntax for palindrome to not print the error statement along with palindrome words

--- assignments/palindrome.sh
+++ assignments/palindrome.sh
@@ -7,10 +7,10 @@ do
   if [[ "$Pal" == "$(rev <<< "$Pal")" ]]
   then
       echo "The palindrome words in the given file are: $Pal"
-  else
-      if [ $? -eq 1 ]
-      then
-      echo "No palindrome words found"
-      fi
+  #else
+  #   if [ $? -eq 1 ]
+  #   then
+  #    echo "No palindrome words found"
+  #    fi
   fi
 done < "$FILE"
