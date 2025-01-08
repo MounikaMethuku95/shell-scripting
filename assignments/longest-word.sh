d8d303d895da1749735bad643a0a3cb7518b7991
mounikamethuku <mounikamethuku95@gmail.com>
2025-01-08 21:15:32 +0530

    updated longest word

--- assignments/longest-word.sh
+++ assignments/longest-word.sh
@@ -2,13 +2,23 @@
 
 longword=0
 
-for lw in $(<strings.txt)
-do
-    len=${#lw}
-    if [[ len -gt longword ]]
-    then 
-        longword=$len
-        longestword=$word
-    fi
+#for lw in $(<strings.txt)
+#do
+#    len=${#lw}
+#    if [[ len -gt longword ]]
+#    then 
+#        longword=$len
+#        longestword=$word
+#    fi
+#done
+#echo " The longest word is  with length $word $longword"
+
+for lw in (i=1;i<=NF;i++)
+do 
+   if [[length($i)>longword]]
+   then
+       longword=length($i)
+       lw=$i
+   fi
 done
-echo " The longest word is  with length $word $longword"
+echo "The longest word is $lw"
