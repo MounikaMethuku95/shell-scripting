fcf80c4bee436294f5be09c2f56bdc29d8f3ca74
mounikamethuku <mounikamethuku95@gmail.com>
2025-01-08 20:13:03 +0530

    modified script for frequency of words

--- assignments/count-occurrences.sh
+++ assignments/count-occurrences.sh
@@ -2,17 +2,20 @@
 
 COUNT="words.txt"
 
-while IFS= read -r  occ 
-do
-  awk '
-     BEGIN { FS="[^a-zA-Z]+" } {
-         for ( occ=1; occ<=NF; occ++) {
-             word = tolower($occ)
-             words[word]++
-         }
-     }
-     END {
-         for (c in words)
-            echo "%3 %s\n, words[w], w"
-     } ' | sort -rn
-done < "$COUNT"
+#while IFS= read -r  occ 
+#do
+#  awk '
+#     BEGIN { FS="[^a-zA-Z]+" } {
+#         for ( occ=1; occ<=NF; occ++) {
+#             word = tolower($occ)
+#             words[word]++
+#         }
+#     }
+#     END {
+#         for (c in words)
+#            echo "%3 %s\n, words[w], w"
+#     } ' | sort -rn
+#done < "$COUNT"
+
+tr '[:upper:]' '[:lower:]' < "$COUNT" | \ tr -c '[:alnum:]' '[\n*]' | \ uniq -c 
+
