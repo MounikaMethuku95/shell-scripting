dd06d69d1979b5a108898f17c6a277ec5a0d2aa0
mounikamethuku <mounikamethuku95@gmail.com>
2025-01-08 18:44:16 +0530

    corrected syntax for palindrome

--- assignments/palindrome.sh
+++ assignments/palindrome.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-FILE=$(<"$1")
+FILE="strings.txt"
 
-if [[ "$FILE" == "$(rev <<< "$FILE")" ]]
-then
-    echo "The palindrome words in the given file are: $FILE"
-else
-    echo "No palindrome words found"
+while IFS= read -r Pal 
+do
+  if [[ "$Pal" == "$(rev <<< "$Pal")" ]]
+  then
+      echo "The palindrome words in the given file are: $Pal"
+  else
+      echo "No palindrome words found"
 fi
