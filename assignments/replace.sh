ad08961f7f61abeb0a3f9d17aa24cfcae54b78ec
mounikamethuku <mounikamethuku95@gmail.com>
2025-01-09 20:39:16 +0530

    added color code for replace word

--- assignments/replace.sh
+++ assignments/replace.sh
@@ -4,11 +4,12 @@ originalword=$1
 replacementword=$2
 
 G="\e[32m"
+B="\e[34m"
 
 file="/c/devops/daws-82s/repos/shell-scripting/assignments/words.txt"
 
 updated_file=$(sed -i "s/$originalword/$replacementword/g" $file)
 
-echo -e $G "Word replacement completed."
+echo -e $G "Word replacement completed." $B
 
 cat $file
