abf960d274ebe8b4d22979f490663808fb44c3ec
mounikamethuku <mounikamethuku95@gmail.com>
2025-01-09 20:28:17 +0530

    replace word

---
+++ assignments/replace.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+originalword=$1
+replacementword=$2
+
+G="\e[32m"
+N="\e[0m"
+
+file="/c/devops/daws-82s/repos/shell-scripting/assignments/words.txt"
+
+updated_file=$(sed -i "s/$originalword/$replacementword/g" $file)
+cat $file
+
+echo -e $G "Word replacement completed" $N
